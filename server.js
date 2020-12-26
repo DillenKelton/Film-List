8b7d7ba97e205a40190bd74e66f133c8d4fa18eb
Dillen Kelton <dillenrindellkelton@gmail.com>
2020-12-26 05:41:23 -0600

    set up basic routing

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const routes = require("./routes");
 
-app.get("/", (req, res) => {
-  res.send("hello world!");
-});
+app.use("/", routes);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
