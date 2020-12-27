6de0f06a732cd32e6851d1851c229c7463cb0e55
Dillen Kelton <dillenrindellkelton@gmail.com>
2020-12-27 15:21:43 -0600

    implimented delete feature

--- routes.js
+++ routes.js
@@ -9,7 +9,21 @@ router.get("/", async (req, res) => {
 router.post("/addFilm", async (req, res) => {
   try {
     const newFilm = await Film.create(req.body);
-    res.send(newFilm);
+    res.status(201).send(newFilm);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const film = await Film.findById(req.params.id);
+    if (film) {
+      await film.delete();
+      res.send(film);
+    } else {
+      res.status(404).send("Film does not exist");
+    }
   } catch (error) {
     res.status(400).send(error);
   }
