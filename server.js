89edb4f6f7110aec19bf3acc4e890399fb9a8e9d
Dillen Kelton <dillenrindellkelton@gmail.com>
2021-01-03 15:22:01 -0600

    ran create-react-app

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // Load environmental variables
 require("dotenv").config();
