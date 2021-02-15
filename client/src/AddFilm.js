713251129c72adb8e79d76f23d1e98a3ce112f57
Dillen Kelton <dillenrindellkelton@gmail.com>
2021-02-15 17:39:42 -0600

    made some style adjustments

--- client/src/AddFilm.js
+++ client/src/AddFilm.js
@@ -4,15 +4,20 @@ import axios from "axios";
 export default class AddFilm extends React.Component {
   render() {
     return (
-      <div>
+      <div class="addFilmForm">
         <label for="filmName">Name: </label>
         <input type="text" id="filmName"></input>
+        <br />
         <label for="releaseYear">Year: </label>
         <input type="text" id="releaseYear"></input>
+        <br />
         <label for="dateWatched">Watched: </label>
         <input type="text" id="dateWatched"></input>
+        <br />
         <label for="blame">Blame: </label>
         <input type="text" id="blame"></input>
+        <br />
+        <button type="button">Add</button>
       </div>
     );
   }
