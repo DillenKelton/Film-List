8a5dba859844300361d2bf7874b1ff2cd1be3328
Dillen Kelton <dillenrindellkelton@gmail.com>
2021-02-15 18:14:55 -0600

    added post to api when adding film

--- client/src/AddFilm.js
+++ client/src/AddFilm.js
@@ -2,22 +2,39 @@ import React from "react";
 import axios from "axios";
 
 export default class AddFilm extends React.Component {
+  addFilmClick() {
+    axios({
+      method: "post",
+      url: "http://72.133.97.71:5000/addFilm",
+      data: {
+        name: document.getElementById("filmName").value,
+        releaseYear: document.getElementById("releaseYear").value,
+        dateWatched: document.getElementById("dateWatched").value,
+        blame: document.getElementById("blame").value,
+        poster: "N/A",
+        tags: [],
+      },
+    });
+  }
+
   render() {
     return (
-      <div class="addFilmForm">
-        <label for="filmName">Name: </label>
+      <div className="addFilmForm">
+        <label htmlFor="filmName">Name: </label>
         <input type="text" id="filmName"></input>
         <br />
-        <label for="releaseYear">Year: </label>
+        <label htmlFor="releaseYear">Year: </label>
         <input type="text" id="releaseYear"></input>
         <br />
-        <label for="dateWatched">Watched: </label>
+        <label htmlFor="dateWatched">Watched: </label>
         <input type="text" id="dateWatched"></input>
         <br />
-        <label for="blame">Blame: </label>
+        <label htmlFor="blame">Blame: </label>
         <input type="text" id="blame"></input>
         <br />
-        <button type="button">Add</button>
+        <button type="button" onClick={this.addFilmClick}>
+          Add
+        </button>
       </div>
     );
   }
